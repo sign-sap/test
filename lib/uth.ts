1a53a89c0fc12bde2e44b27f9a069f711f7751b5
signature-sap <w434920439@gmail.com>
2026-01-15 23:32:52 +0300

    Fix formatting and ensure user is authenticated

--- lib/uth.ts
+++ lib/uth.ts
@@ -2,9 +2,11 @@ import { getSessionUser } from './session'
 
 export async function requireAuth() {
   const user = await getSessionUser()
+  
   if (!user) {
     throw new Error('UNAUTHORIZED')
   }
+  
   return user
 }
 
