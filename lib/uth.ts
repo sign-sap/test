06b66e6da1ca4229eea6e9b452f5f5c0d653a2af
signature-sap <w434920439@gmail.com>
2026-01-15 23:14:05 +0300

    Create uth.ts

---
+++ lib/uth.ts
@@ -0,0 +1,11 @@
+import { getSessionUser } from './session'
+
+export async function requireAuth() {
+  const user = await getSessionUser()
+  if (!user) {
+    throw new Error('UNAUTHORIZED')
+  }
+  return user
+}
+
+export { getSessionUser }
